84d5b5fbab6ae704802ceb2f85afd3891ce93b64
Nionik <Patrycja.pl.opalacz@gmail.com>
2022-02-16 11:31:39 +0100

    Mam problem

--- canvas.js
+++ canvas.js
@@ -1,5 +1,17 @@
 var canvas = document.querySelector('canvas');
 
 
-var c = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+
+
 console.log(canvas);
+ctx.beginPath();
+ctx.moveTo(0, canvas.height / 2); //zaczniemy rysowanie od x: 0, y: polowaWysokosci
+const step = 10; //krok co 10
+const howMany = canvas.width / step; //ile kroków
+
+for (let i = 1; i <= howMany; i++) {
+    const y = canvas.height / 2 + rand(-50, 50);
+    ctx.lineTo(i * step, y)
+}
+ctx.stroke();
