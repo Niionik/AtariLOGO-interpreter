17f556c2fc43f87090085773af644da34a5d42fd
Nionik <Patrycja.pl.opalacz@gmail.com>
2022-02-19 21:50:54 +0100

    Poprawiam pełzacza

--- canvas.js
+++ canvas.js
@@ -1,10 +1,17 @@
 var c = document.getElementById("myCanvas");
 
+
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 var ctx = c.getContext("2d");
 var img = document.getElementById("rzut1");
-ctx.drawImage(img, 900, 700);
+//ctx.drawImage(img, 900, 700);
+
+
+onload:
+    img.onload = function() {
+        ctx.drawImage(img, (c.width / 2) - (img.offsetWidth / 2), (c.height / 2) - (img.offsetHeight / 2));
+    }
 
 function draw() {
 
