586a00209145b611069537be72c4df37f02980e9
Jakyubi <frostwth.xd@gmail.com>
2022-02-25 18:31:39 +0100

    komentarze

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function remove(){
     document.querySelector("#text_area").value = null;
     }
-    
+    //funkcja do czyszczenia przyciskiem w index.html pola tekstowego gdzie sie wpisuje komendy (nwm po co to komu, szybciej jest ctrl+a i backspace ale ok)
