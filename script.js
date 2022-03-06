80cd678572580e72afe363043402655185718a25
Jakyubi <frostwth.xd@gmail.com>
2022-03-06 22:28:43 +0100

    poprawki

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ function remove(){
     document.querySelector("#text_area").value = null;
     }
 
+    function wpisz(){
+        document.querySelector("#text_area").value = a;
+
+    }
+    console.log(a)
+
+    function kolo(){
+        document.querySelector("#text_area").value = document.write="fd 100 rt 144 fd 100 rt 144 fd 100 rt 144 fd 100 rt 144 fd 100 rt 144";
+        }
+    function kwadrat(){
+         document.querySelector("#text_area").value = document.write="fd 100 rt 90 fd 100 rt 90 fd 100 rt 90 fd 100 rt 90";
+        }
+    function trojkat(){
+        document.querySelector("#text_area").value = document.write="rt 45 fd 100 rt 90 fd 100 rt 135 fd 150";
+        }
+    function pro(){
+        document.querySelector("#text_area").value = document.write="fd 100 rt 90 fd 300 rt 90 fd 100 rt 90 fd 300 rt 90";
+        }      
+        function trapez(){
+                document.querySelector("#text_area").value = document.write="rt 30 fd 100 rt 60 fd 100 rt 60 fd 100 rt 300 fd 150";
+        }                  
+        function replace(){
+            document.getElementById("inf").interHTML="";
+
+        }
+
+        function inst(){
+            document.getElementById("inf").innerHTML="<centre><p>Przygotowaliśmy instrukcje komend oraz gotowe zestawy</p>fd - rzuf idzie<br>rt - obrót o *kąt* w prawo<br>jp = rzuf skacze<br>ht - chowa rzufia</p></centre>";
+        }
